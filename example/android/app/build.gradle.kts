21dc6873c1b012f27c3f731be6d9d0d4d56eb26b
Thomas <thomas.nguyen@pimspoints.com>
2025-11-22 16:37:50 +0700

    change name

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.calendar_with_callback_example"
+    namespace = "com.quanhh.calendar_with_callback_example"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.calendar_with_callback_example"
+        applicationId = "com.quanhh.calendar_with_callback_example"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
